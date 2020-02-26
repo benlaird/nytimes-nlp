b8ecbcaba24d4920290bb17e8e2026e089e0661a
Ben Johnson-Laird <benjohnsonlaird@gmail.com>
2020-02-25 20:36:57 -0500

    Added various functions for cleanup, save & read

--- getDataFromNYTimesAPI.py
+++ getDataFromNYTimesAPI.py
@@ -94,6 +94,7 @@ def get_op_eds_for_date(date_str):
     results_processed = 0
     page = 0
     hits = None
+    new_articles = []
 
     while hits is None or results_processed < hits:
 
@@ -117,8 +118,10 @@ def get_op_eds_for_date(date_str):
             art_obj.set_article_text()
             # Get all the comments for the article
             get_comments_for_article(art['web_url'])
+            new_articles.append(art_obj)
 
         page += 1
+    return new_articles
 
 
 def iterate_thru_dates():
@@ -134,7 +137,7 @@ def iterate_thru_dates():
         curr_dt = curr_dt + timedelta(days=1)
 
 
-def save_training_data(start_date, end_date):
+def save_training_data(start_date):
     """
     1) Get all the articles in the date range
     2) Get all the comments for the articles in the date range
@@ -149,22 +152,19 @@ def save_training_data(start_date, end_date):
     filename_for_comments = f"data/trainingComments-{start_date}.json"
 
     curr_dt = datetime.strptime(start_date, '%Y-%m-%d')
-    end_dt = datetime.strptime(end_date, '%Y-%m-%d')
 
-    while curr_dt <= end_dt:
-        curr_day = datetime.strftime(curr_dt, '%Y-%m-%d')
-        print(f"Curr day is: {curr_day}")
-        get_op_eds_for_date(curr_day)
-        curr_dt = curr_dt + timedelta(days=1)
+    curr_day = datetime.strftime(curr_dt, '%Y-%m-%d')
+    print(f"Curr day is: {curr_day}")
+    arts = get_op_eds_for_date(curr_day)
 
     # Save the articles to file
-    timesArticle.TimesArticle.save_to_json(filename_for_articles)
+    timesArticle.TimesArticle.save_to_json(filename_for_articles, filename_for_comments, arts)
 
     # Save the comments to file
-    timesComment.TimesComment.save_to_json(filename_for_comments)
+    # timesComment.TimesComment.save_to_json(filename_for_comments)
 
 
-timesArticle.TimesArticle.read_from_json(f"data/trainingArticles-2020-02-10.json")
+# timesArticle.TimesArticle.read_from_json(f"data/trainingArticles-2020-02-10.json")
 # get_comments_for_article('https://www.nytimes.com/2020/02/10/opinion/trillion-trees-trump-climate.html')
 
 # timesComment.TimesComment.read_from_json('data/testComments.json')
@@ -172,5 +172,133 @@ timesArticle.TimesArticle.read_from_json(f"data/trainingArticles-2020-02-10.json
 
 # get_op_eds_for_date("2020-02-18")
 
-# get_training_data("2020-01-18", "2020-01-18")
-print("Got here")
+# save_training_data("2020-01-12")
+
+"""
+article = timesArticle.TimesArticle._article_map[new_comment.article_url]
+if article:
+    article.add_comment(new_comment)
+else:
+    raise Exception(f"No article for comment_id: {new_comment.comment_id}"
+                    f" article_url: {new_comment.article_url}")
+"""
+
+
+def clean_up_files(start_date, end_date=None):
+    """
+    For each day, read in the file.. Extract just the data for today, both the comments
+    and the article. And then resave them.
+    :return:
+    """
+    if end_date is None:
+        end_date = start_date
+
+    curr_dt = datetime.strptime(start_date, '%Y-%m-%d')
+    end_dt = datetime.strptime(end_date, '%Y-%m-%d')
+
+    while curr_dt <= end_dt:
+        curr_day = datetime.strftime(curr_dt, '%Y-%m-%d')
+        url_day = datetime.strftime(curr_dt, '%Y/%m/%d')
+        print(f"Curr day is: {curr_day}")
+
+        old_dir = "data.old/"
+        new_dir = "data/"
+        filename_for_articles = f"trainingArticles-{curr_day}.json"
+        filename_for_comments = f"trainingComments-{curr_day}.json"
+
+        arts = timesArticle.TimesArticle.read_from_json(f"{old_dir}/{filename_for_articles}")
+        comments = timesComment.TimesComment.read_from_json(f"{old_dir}{filename_for_comments}")
+
+        clean_arts = []
+        clear_arts_urls = []
+        found = False
+        for a in arts:
+            pub_date = a.pub_date[0:10]
+            if curr_day == pub_date:
+                print(f"Found: {a.web_url}")
+                clean_arts.append(a)
+                clear_arts_urls.append(a.web_url)
+                found = True
+            else:
+                print(f"skipping: {a.web_url}")
+
+        clean_comments = []
+        for c in comments:
+            if c.article_url in clear_arts_urls:
+                clean_comments.append(c)
+
+        print(f"Total articles: {len(arts)} clean articles: {len(clean_arts)}")
+        print(f"Total comments: {len(comments)} clean comments: {len(clean_comments)}")
+
+        # Save the articles to file
+        timesArticle.TimesArticle.save_to_json(f"{new_dir}/{filename_for_articles}", clean_arts)
+
+        # Save the comments to file
+        timesComment.TimesComment.save_to_json(f"{new_dir}{filename_for_comments}", clean_comments)
+
+
+def save_articles(articles, new_dir, start_date, end_date=None):
+
+    if end_date is None:
+        end_date = start_date
+
+    curr_dt = datetime.strptime(start_date, '%Y-%m-%d')
+    end_dt = datetime.strptime(end_date, '%Y-%m-%d')
+
+    while curr_dt <= end_dt:
+        curr_day = datetime.strftime(curr_dt, '%Y-%m-%d')
+        url_day = datetime.strftime(curr_dt, '%Y/%m/%d')
+        print(f"Curr day is: {curr_day}")
+
+        filename_for_articles = f"trainingArticles-{curr_day}.json"
+
+        clean_arts = []
+        for a in articles:
+            pub_date = a.pub_date[0:10]
+            if curr_day == pub_date:
+                clean_arts.append(a)
+
+        # Save the articles to file
+        timesArticle.TimesArticle.save_to_json(f"{new_dir}/{filename_for_articles}", clean_arts)
+
+        curr_dt = curr_dt + timedelta(days=1)
+
+
+def read_articles(start_date, end_date=None):
+    """
+    For each day, read in the file.. Extract just the data for today, both the comments
+    and the article. And then resave them.
+    :return:
+    """
+    if end_date is None:
+        end_date = start_date
+
+    timesArticle.TimesArticle._articles = []
+    curr_dt = datetime.strptime(start_date, '%Y-%m-%d')
+    end_dt = datetime.strptime(end_date, '%Y-%m-%d')
+    all_arts = []
+    running_total = 0
+
+    while curr_dt <= end_dt:
+        curr_day = datetime.strftime(curr_dt, '%Y-%m-%d')
+        url_day = datetime.strftime(curr_dt, '%Y/%m/%d')
+        print(f"Curr day is: {curr_day}")
+
+        filename_for_articles = f"trainingArticles-{curr_day}.json"
+        filename_for_comments = f"trainingComments-{curr_day}.json"
+
+        arts = timesArticle.TimesArticle.read_from_json(f"{config.global_data_read_dir}/{filename_for_articles}")
+        comments = timesComment.TimesComment.read_from_json(f"{config.global_data_read_dir}/{filename_for_comments}")
+        running_total += len(arts)
+        print(f"Total articles: {len(arts)}")
+        print(f"Running total: {running_total}")
+        print(f"Total comments: {len(comments)}")
+
+        curr_dt = curr_dt + timedelta(days=1)
+
+    return timesArticle.TimesArticle._articles
+
+
+# arts = read_articles(config.global_start_date, config.global_end_date)
+# print(len(arts))
+
