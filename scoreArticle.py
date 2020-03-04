c7753532871dc5155e3cbdf8196b36bb6da3ad58
Ben Johnson-Laird <benjohnsonlaird@gmail.com>
2020-03-04 09:13:44 -0500

    make number of quantiles a parameter, for generating scores

--- scoreArticle.py
+++ scoreArticle.py
@@ -1,4 +1,7 @@
 import time
+
+import pandas as pd
+import numpy as np
 import requests
 import json
 from collections import namedtuple
@@ -16,7 +19,7 @@ import config
 # Display number of votes in each percentiles
 
 
-def top_n_comments_by_recommendations(start_date, end_date):
+def top_n_comments_by_recommendations_old(start_date, end_date):
     """
     Returns the top_n comments by recommendations... currently only works for the same start & end_date
     :param end_date:
@@ -31,7 +34,7 @@ def top_n_comments_by_recommendations(start_date, end_date):
         curr_day = datetime.strftime(curr_dt, '%Y-%m-%d')
         print(f"Curr day is: {curr_day}")
 
-        filename_for_articles = f"${config.global_data_read_dir}/trainingArticles-{start_date}.json"
+        filename_for_articles = f"{config.global_data_read_dir}/trainingArticles-{start_date}.json"
         filename_for_comments = f"{config.global_data_read_dir}/trainingComments-{start_date}.json"
         print(f"Filename for articles: {filename_for_articles} filename_for_comments: {filename_for_comments}")
         curr_dt = curr_dt + timedelta(days=1)
@@ -51,6 +54,28 @@ def top_n_comments_by_recommendations(start_date, end_date):
             # Top 10
             print(sorted_comms[0:10])
         return sorted_comms[0:10]
+
+
+def top_n_comments_by_recommendations(arts):
+    """
+    Returns the top_n comments by recommendations...
+    :param arts:
+    :return:
+    """
+    top_n_comments = []
+
+    # Find the most popular comments
+    for a in arts:
+        total_recommendations = 0
+        for c in a.comments:
+            total_recommendations += c.recommendations
+        print(f"Total recommendations: {total_recommendations}")
+        sorted_comms = sorted(a.comments, key=lambda x: x.recommendations, reverse=True)
+        # Top 10
+        top_n = sorted_comms[0:10]
+        top_n_comments.extend(top_n)
+
+    return top_n_comments
 
 
 def compute_total_comments_by_article(start_date, end_date):
@@ -89,8 +114,10 @@ def compute_total_comments_by_article(start_date, end_date):
     return num_comments_list, arts_with_comments
 
 
-def code_data_by_quantile(start_date, end_date):
+def code_data_by_quantile(start_date, end_date, num_quantiles, save_files=False):
     """
+     :param save_files:
+     :param num_quantiles:
      :param end_date:
      :param start_date:
      :return:  add the quantile prediction to each article
@@ -100,61 +127,44 @@ def code_data_by_quantile(start_date, end_date):
             < Q1 == 4, >= Q1 & < Q2 == 3
             >= Q2 & < Q3 == 2
             >= Q3 == 1
-        Q1 (25%) quantile of arr :  2908.0  -- 
-        Q2 (median) quantile of arr :  8819.0
-        Q3 (75%) quantile of arr :  18033.0
+        q1 = 190.0
+        q2 = 405.0
+        q3 = 725.0
     """
-    q1 = 190.0
-    q2 = 405.0
-    q3 = 725.0
-
-    save_files = False
     curr_dt = datetime.strptime(start_date, '%Y-%m-%d')
     end_dt = datetime.strptime(end_date, '%Y-%m-%d')
     arts_to_save = []
 
-    while curr_dt <= end_dt:
-        curr_day = datetime.strftime(curr_dt, '%Y-%m-%d')
-        print(f"Curr day is: {curr_day}")
+    arts = getDataFromNYTimesAPI.read_data()
+    getDataFromNYTimesAPI.clean_data(arts)
+    sort_comments = [a.num_comments for a in arts]
+    quantiles = pd.qcut(sort_comments, num_quantiles, np.arange(num_quantiles, 0, -1))
 
-        filename_for_articles = f"data/trainingArticles-{curr_day}.json"
-        filename_for_comments = f"data/trainingComments-{curr_day}.json"
-        print(f"Filename for articles: {filename_for_articles} filename_for_comments: {filename_for_comments}")
-        curr_dt = curr_dt + timedelta(days=1)
+    # Find the most popular comments
+    i = 0
+    for a in arts:
+        # Skip articles with zero comments
+        if a.num_comments == 0:
+            continue
+        # if i != 51 and i != 72 and i != 106 and a.popularity_quantile != quantiles[i]:
+        #    raise Exception("old and new quantile values are not equal")
+        a.popularity_quantile = int(quantiles[i])
+        print(f"{a.web_url} Total comments: {a.num_comments} popularity: {a.popularity_quantile}")
 
-        arts = timesArticle.TimesArticle.read_from_json(filename_for_articles)
-        timesComment.TimesComment.read_from_json(filename_for_comments)
-        print(f"Read {len(arts)} articles for {curr_day}")
-
-        # Find the most popular comments
-        for a in arts:
-            # Skip articles with zero comments
-            if a.num_comments == 0:
-                continue
-            print(f"{a.web_url} Total comments: {a.num_comments}")
-            if a.num_comments < q1:
-                a.popularity_quantile = 4
-            elif a.num_comments >= q1 and a.num_comments < q2:
-                a.popularity_quantile = 3
-            elif a.num_comments >= q2 and a.num_comments < q3:
-                a.popularity_quantile = 2
-            else:
-                a.popularity_quantile = 1
-            print(f"{a.web_url} Total comments: {a.num_comments} popularity: {a.popularity_quantile}")
-
-            print(a)
-            arts_to_save.append(a)
+        print(a)
+        arts_to_save.append(a)
+        i += 1
 
     if save_files:
         print(f"Saving: {len(arts_to_save)} articles")
-        getDataFromNYTimesAPI.save_articles(arts_to_save, "data2", config.global_start_date, config.global_end_date)
+        getDataFromNYTimesAPI.save_articles(arts_to_save, "data3", config.global_start_date, config.global_end_date)
 
 
 # compute_comment_score("2020-01-12", "2020-01-12")
 # recs = compute_recommendations_by_article("2020-01-12", "2020-01-12")
 # print(recs)
 
-# code_data_by_quantile(config.global_start_date, config.global_end_date)
+code_data_by_quantile(config.global_start_date, config.global_end_date, 20, save_files=True)
 
 
 
